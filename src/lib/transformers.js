63dc5e2f64752b4ca2881751d1474b25b7ddeb97
Adam Terlson <adam.terlson@internations.org>
2016-02-19 10:51:02 +0100

    Variables transformer

--- src/lib/transformers.js
+++ src/lib/transformers.js
@@ -3,9 +3,39 @@ export function combineTransformers(...transformers) {
     throw new Error('No transformers provided');
   }
 
-  return (sheet) => 
+  return (sheet) =>
     transformers.reduce(
-      (result, transformer) => transformer(result), 
+      (result, transformer) => transformer(result),
       sheet
     );
 };
+
+export function variables(variables) {
+    let regex = /\$([\w]*)/g;
+    let match;
+
+    return styles => {
+        Object.keys(styles).forEach(selector => {
+            let attributes = styles[selector];
+            Object.keys(attributes).forEach(attribute => {
+                let value = attributes[attribute];
+
+                if (typeof value !== 'string') {
+                    return;
+                }
+                if (value.indexOf('$') === -1) {
+                    return;
+                }
+
+                while (match = regex.exec(value)) {
+                    let [variableString, variableName] = match;
+                    value = value.replace(variableString, variables[variableName]);
+                }
+
+                attributes[attribute] = value;
+            });
+        });
+
+        return styles;
+    };
+}
