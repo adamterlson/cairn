a8482e197ecb7d32526ce4305996832440778576
Adam Terlson <adam.terlson@gmail.com>
2016-03-05 20:06:43 +0100

    Variable support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { default as cairn } from './lib/factory';
-import { compose } from './lib/transformers';
+import { compose, functional, variables } from './lib/transformers';
 import style from './lib/style';
 import pile from './lib/pile';
 
 cairn.style = style;
 cairn.pile = pile;
 cairn.compose = compose;
+cairn.functional = functional;
+cairn.variables = variables;
 
 export default cairn;
--- src/lib/transformers.js
+++ src/lib/transformers.js
@@ -1,3 +1,15 @@
+/**
+ * Combine together multiple transformers into one
+ *
+ * compose(
+ *   transformerOne,
+ *   transformerTwo,
+ *   compose(
+ *     transformerThree,
+ *     transformerFour
+ *   )
+ * )
+ */
 export function compose(...transformers) {
   if (!transformers || !transformers.length) {
     throw new Error('No transformers provided');
@@ -10,32 +22,73 @@ export function compose(...transformers) {
     );
 };
 
+/**
+ * Add support for variables in the form:
+ * 
+ * {
+ *   prop: '$variable'
+ * }
+ */
+
 export function variables(variables) {
-    let regex = /\$([\w]*)/g;
-    let match;
+  if (!variables) {
+    throw new Error('No variables provided');
+  }
 
-    return styles => {
-        Object.keys(styles).forEach(selector => {
-            let attributes = styles[selector];
-            Object.keys(attributes).forEach(attribute => {
-                let value = attributes[attribute];
+  function getVariableValue(value) {
+    const reg =  /\$([\w]*)/;
+    const [fullMatch, variableName] = reg.exec(value);
+    const varValue = variables[variableName];
 
-                if (typeof value !== 'string') {
-                    return;
-                }
-                if (value.indexOf('$') === -1) {
-                    return;
-                }
+    // Variable present, but not declared, return original
+    if (varValue == null) {
+      return value;
+    }
 
-                while (match = regex.exec(value)) {
-                    let [variableString, variableName] = match;
-                    value = value.replace(variableString, variables[variableName]);
-                }
+    // The variable is the only thing, use the variable as the value
+    if (fullMatch.length === value.length) {
+      return varValue;
+    }
 
-                attributes[attribute] = value;
-            });
-        });
+    // There's other stringy stuff, just replace the value in the string
+    return value.replace(fullMatch, varValue);
+  }
 
-        return styles;
-    };
+  return styles => {
+    Object.keys(styles).forEach(selector => {
+      const attributes = styles[selector];
+
+      Object.keys(attributes).forEach(attribute => {
+        const value = attributes[attribute];
+
+        // It's not a string, bail
+        if (typeof value !== 'string') {
+          return;
+        }
+        
+        // No variables present in said string, bail
+        if (value.indexOf('$') === -1) {
+          return;
+        }
+
+        attributes[attribute] = getVariableValue(value);
+      });
+    });
+
+    return styles;
+  };
 }
+
+/**
+ * Style function values are executed at pile time
+ */
+export function functional(...args) {
+  return (sheet) => Object.keys(sheet).reduce((result, key) => {
+    if (typeof sheet[key] === 'function') {
+      result[key] = sheet[key](...args);
+    } else {
+      result[key] = sheet[key];
+    }
+    return result;
+  }, {});
+};
